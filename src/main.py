b4b729966dd7574406dca79628b2ef16a4cfbe6f
gavin <blazingriver449@gmail.com>
2025-07-02 00:02:01 -0600

    fix: Added support for replit and some other non windows systems.

--- src/main.py
+++ src/main.py
@@ -82,13 +82,13 @@ from entity import Entity
 import pygame
 import time
 from collections import deque
-import ctypes
 
-
+pygame.init()
 pygame.font.init()
 
-user32 = ctypes.windll.user32
-screen_width, screen_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+info = pygame.display.Info()
+screen_width, screen_height = info.current_w, info.current_h
+
 WIDTH, HEIGHT = (
     screen_width // SCREEN_SCALE_FACTOR,
     screen_height // SCREEN_SCALE_FACTOR,
